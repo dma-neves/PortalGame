1f64e603a900b8b6b4392a28c546fc4f70a5da9c
Daweed <david.m.neves@live.com>
2019-07-02 15:42:38 +0100

    Implemented launcher

---
+++ Launcher/launcher.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#define INSTALL_LIBS "sudo apt-get install libsfml-dev"
+#define BUILD "cd Game; make; cd .."
+#define RUN_GAME "cd Game/bin; ./PortalGame.out; cd ../.."
+#define RUN_EDITOR "cd LevelEditor/src; python app.py; cd ../.."
+
+void runCommand(char* cmd)
+{
+    std::cout << "Command: "  << cmd<< std::endl << std::endl;
+    system(cmd);
+}
+
+int main()
+{
+    int input = 0;
+
+    while(input != 4)
+    {
+        std::cout << std::endl << "0) Install libs" << std::endl;
+        std::cout << "1) Build program" << std::endl;
+        std::cout << "2) Run game" << std::endl;
+        std::cout << "3) Run editor" << std::endl;
+        std::cout << "4) exit" << std::endl << std::endl;
+
+        std::cin >> input;
+        switch(input)
+        {
+            case 0: runCommand(INSTALL_LIBS); break;
+            case 1: runCommand(BUILD); break;
+            case 2: runCommand(RUN_GAME); break;
+            case 3: runCommand(RUN_EDITOR); break;
+        }
+    }
+}
---
+++ LevelEditor/src/app.py
@@ -0,0 +1,19 @@
+def main():
+    validInput = False
+
+    while not validInput:
+        print("<l> - Load level")
+        print("<n> - Creat new level")
+
+        cmd = raw_input()
+
+        if cmd == "l" or cmd == "n":
+            levelName = raw_input("Level name: ")
+
+        file = levelName + ".txt"
+
+        print("\nFile: " + file)
+        validInput = True
+
+main()
+
